8c4760a842775487a17983618eb3a7418a34b329
Circus-Pygargus <richard.meuret@laposte.net>
2022-06-12 18:40:22 +0200

    [FEAT] Route app_antipassoire_show + modif ckeditor (ajout code snippets)

---
+++ src/Controller/AntiPassoireController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AntiPassoireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AntiPassoireController extends AbstractController
+{
+    /**
+     * @Route("/anti-passoire/{slug}", name="app_antipassoire_show")
+     */
+    public function show(
+        AntiPassoireRepository $antiPassoireRepository,
+        string $slug
+    ): Response
+    {
+        $antiPassoire = $antiPassoireRepository->findOneBy(['slug' => $slug]);
+
+        if ($antiPassoire === null) {
+            $this->addFlash('error', "Un problème est survenu, l'anti passoire n'a pas été créée !");
+            $return = $this->redirectToRoute('admin_antipassoire_list');
+        } else {
+            $return = $this->render('anti_passoire/index.html.twig', [
+                'antiPassoire' => $antiPassoire
+            ]);
+        }
+        
+        return $return;
+    }
+}
