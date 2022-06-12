d564ae6ee8f2040cc3aef41cb77435630acfec8e
Circus-Pygargus <richard.meuret@laposte.net>
2022-06-12 15:33:47 +0200

    [FEAT] Ajout de la recherche d'antiPassoires par mots clés + affichage résultat minimaliste

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\SearchAntiPassoireType;
+use App\Repository\AntiPassoireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +14,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(
+        Request $request,
+        AntiPassoireRepository $antiPassoireRepository
+    ): Response
     {
+        $antiPassoires = [];
+        
+        $form = $this->createForm(SearchAntiPassoireType::class);
+        $search = $form->handleRequest($request);
+//dd($search->get('categories')->getData());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $antiPassoires = $antiPassoireRepository->search(
+                $search->get('keyWords')->getData(),
+                $search->get('category')->getData()
+            );
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
+            'antiPassoires' => $antiPassoires 
         ]);
     }
 }
