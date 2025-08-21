2f8f0347c6a112aee7c9d3e182066f1c18e2710b
Circus-Pygargus <richard.meuret@laposte.net>
2025-08-21 13:53:41 +0200

    [REFACTO] Déplacement du code relatif au searcher de custom-select.js dans searcher.js

--- assets/js/searcher/searcher.js
+++ assets/js/searcher/searcher.js
@@ -2,3 +2,24 @@
 
 // js
 import './pagination';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Il y a des * juste après les labels des boutons radio
+    const radioButtonsLabels = document.querySelectorAll('.radio-group div label');
+    radioButtonsLabels.forEach(radioBtnLabel => {
+        radioBtnLabel.classList.remove('required');
+    });
+
+    // actions du bouton d'affichage ou non des options de recherches avancées (max par page, trier par, ordre d'affichage)
+    const searcher = document.querySelector('.search');
+    if (searcher) {
+        const displayerBtn = document.querySelector('.displayer-btn');
+        const hideableDisplayer = searcher.querySelector('.hideable');
+        displayerBtn.addEventListener('click', () => {
+            searcher.classList.toggle('opened');
+            hideableDisplayer.classList.toggle('hidden');
+        });
+    }
+});
